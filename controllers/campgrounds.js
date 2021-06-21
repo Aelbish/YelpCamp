c5acea868cec94fa82eb36f86d11c2b480fc8c91
Aelbish Shrestha <aelbish@gmail.com>
2021-06-21 00:30:58 -0500

    added mapbox for geocoding

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -2,6 +2,11 @@
 const Campground = require("../models/campground");
 const { cloudinary } = require("../cloudinary");
 
+//npm i @mapbox/mapbox-sdk
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
+
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index.ejs", { campgrounds });
@@ -12,16 +17,24 @@ module.exports.renderNewForm = (req, res) => {
 };
 
 module.exports.createCampground = async (req, res, next) => {
-  const campground = new Campground(req.body.campground);
-  //map over the array that is added to req.files and add those objects having path and filename in the images array of the model
-  campground.images = req.files.map((f) => ({
-    url: f.path,
-    filename: f.filename,
-  }));
-  campground.author = req.user._id;
-  await campground.save();
-  req.flash("success", "Successfully made a new campground!");
-  res.redirect(`/campgrounds/${campground._id}`);
+  //mapbox
+  const geoData = await geocoder
+    .forwardGeocode({
+      query: req.body.campground.location,
+      limit: 1,
+    })
+    .send();
+  res.send(geoData.body.features[0].geometry.coordinates);
+  // const campground = new Campground(req.body.campground);
+  // //map over the array that is added to req.files and add those objects having path and filename in the images array of the model
+  // campground.images = req.files.map((f) => ({
+  //   url: f.path,
+  //   filename: f.filename,
+  // }));
+  // campground.author = req.user._id;
+  // await campground.save();
+  // req.flash("success", "Successfully made a new campground!");
+  // res.redirect(`/campgrounds/${campground._id}`);
 };
 
 module.exports.showCampground = async (req, res) => {
